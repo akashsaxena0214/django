ce5d4dd997e62a41bcc9a0a4f40360f2c02d50b0
akashsaxena0214 <akashsaxena0214@gmail.com>
2025-12-11 23:23:51 +0530

    Add files via upload

---
+++ personal_website/personal_website/pages/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home_page_view(request):
+    return render(request, "pages/home.html", {})
+
+def about_page_view(request):
+    return render(request, "pages/about.html", {})
