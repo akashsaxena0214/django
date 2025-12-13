5c51184caa60872ef2b5357c18f15dc9d56f2570
akashsaxena0214 <akashsaxena0214@gmail.com>
2025-12-13 13:35:18 +0530

    Delete chapter 6/blog d

--- chapter 6/blog/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import post_list, post_detail
-
-urlpatterns = [
-    path("", post_list, name="home"),
-    path("post/<int:pk>/", post_detail, name="post_detail"),
-]
